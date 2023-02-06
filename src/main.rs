029394206c6a4b00a19cc7674f591c2be57eb65a
Ayush <62958067+Ayush-v@users.noreply.github.com>
2023-02-07 01:34:22 +0530

    mutable types and input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// to remove error like variables which are not used (normally dont use it)
+#![allow(unused)]
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, ErrorKind, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    let mut name: String = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Didn't Receive input");
+    println!("Hello, {}! {}", name.trim_end(), greeting);
 }
